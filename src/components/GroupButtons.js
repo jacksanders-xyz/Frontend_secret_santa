84f6cd11ac755afb0bea8df0f18eb4600bbaf610
Jack Sanders <Guitarbreakerjs@gmail.com>
2020-12-06 23:36:56 -0700

    first forms

--- src/components/GroupButtons.js
+++ src/components/GroupButtons.js
@@ -2,9 +2,43 @@ import { Component } from 'react';
 
 class GroupButtons extends Component {
     
+  state = {
+    name: "",
+    password:  "",
+    drawDate: "",
+    gameDay: ""
+  }
+  
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.signUp(this.state)
+  }
+  
+  handleChange = (event) => {
+    this.setState({
+    [event.target.name]: event.target.value
+    })
+  }
+
   render() {
     return (
-     <h1>GroupButtons</h1> 
+      <form onSubmit={this.handleSubmit} className="group-form">
+          <h1>Sign Up a group!</h1>
+          
+          <label>Group Name</label>
+          <input name="name" value={this.state.name} onChange={this.handleChange}/>
+         
+          <label>Password</label>
+          <input type="password" name="password" value={this.state.password} onChange={this.handleChange}/>
+         
+          <label>drawDate</label>
+          <input name="drawDate" value={this.state.drawDate} onChange={this.handleChange}/>
+         
+          <label>gameDay</label>
+          <input name="gameDay" value={this.state.gameDay} onChange={this.handleChange}/>
+          
+          <input type="submit" value="Create Group" />
+      </form>
     );
   }
 }
--- src/components/UserButtons.js
+++ src/components/UserButtons.js
@@ -1,10 +1,50 @@
 import { Component } from 'react';
 
 class UserButtons extends Component {
-    
+
+  state = {
+    firstName: "",
+    lastName:  "",
+    groupId: "",
+    interests: ""
+  }
+  
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.signUp(this.state)
+  }
+  
+  handleChange = (event) => {
+    this.setState({
+    [event.target.name]: event.target.value
+    })
+  }
+   
+
+
+
+
+
+
   render() {
     return (
-     <h1>User Buttons</h1>   
+      <form onSubmit={this.handleSubmit} className="user-form">
+          <h1>Sign Up!</h1>
+          
+          <label>First Name</label>
+          <input className="myWidth" name="firstName" value={this.state.firstName} onChange={this.handleChange}/>
+          
+          <label>Last Name</label>
+          <input className="myWidth" name="lastName" value={this.state.lastName} onChange={this.handleChange}/>
+          
+          <label>GroupId</label>
+          <input className="myWidth" name="groupId" value={this.state.groupId} onChange={this.handleChange}/>
+          
+          <label>interests</label>
+          <textarea className="interests-field" name="interests" value={this.state.interests} onChange={this.handleChange}/>
+          
+          <input className="create-user-button" type="submit" value="Create User" />
+      </form>
     );
   }
 }
